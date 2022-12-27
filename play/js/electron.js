f7f476110c33ad896ca2a7214d091d2f0166f38c
飞机上的长电视 <kevin201609@foxmail.com>
2022-12-27 18:02:02 +0800

    rnmb

--- play/js/electron.js
+++
@@ -1,7 +0,0 @@
-const electron = new UAParser().getBrowser().name === "Electron" ? require("electron") : null;
-const ipcRenderer = electron ? electron.ipcRenderer : null;
-
-function cacheObject(key, value) {
-    if (!electron) return null;
-    
-}
